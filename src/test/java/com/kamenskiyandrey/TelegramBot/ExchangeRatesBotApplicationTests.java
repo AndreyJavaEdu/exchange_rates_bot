476b17526b78a86e95ce4cf92e004edf3d0002b6
AndreyJavaEd <scream123456@yandex.ru>
2024-01-03 20:18:01 +0300

    Initial commit

---
+++ src/test/java/com/kamenskiyandrey/TelegramBot/ExchangeRatesBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamenskiyandrey.TelegramBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRatesBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
